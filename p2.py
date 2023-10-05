aec8c91be625d14b71095479ea79bb09b2981ea9
bibek karki <bibek.191613@ncit.edu.np>
2023-10-05 18:38:42 +0545

    master

--- p2.py
+++ p2.py
@@ -1,1 +1,33 @@
 print("hello woorld")
+# x=bool(5)
+# print(type(x))
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+# print(x + y + z)
+# print(x +" " +y +" "+ z)
+# x = "awesome" #gobal variable
+
+# def myfunc():
+#   print("Python is " + x)
+
+# myfunc()
+x = "awesome"  #globalvariable
+
+def myfunc():
+  x = "fantastic"  #localvariable
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+# If you use the global keyword, the variable belongs to the global scope:
+
+def myfunc():
+  global y
+  y = "fantastic"
+
+myfunc()
+
+print("Python is " + y)
